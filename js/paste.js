61fdf7fb13e2c48086cb41ac9ffec3373a6ab945
yulu04 <yulu04@baidu.com>
2018-03-21 10:25:31 +0800

    插入图像打下优化

--- js/paste.js
+++ js/paste.js
@@ -14,20 +14,33 @@ $(document).ready(function() {
             }      
         }
 
-        if (blob !== null) {        
-            // 图像读取完成，执行回调     
-            var reader = new FileReader();
-            reader.onload = function(event) {           
-                var buffer = new Buffer(reader.result)
-                sendFile(buffer, function(data) {
-                    if (data.code == 0 && data.image_url) {
-                        document.execCommand('insertimage', false, data.image_url);
-                    }
-                })
-            }
+        if (blob !== null) {
+            // 获取图像信息
+            var img = new Image();
+            img.onload = function(){
+                var width = this.width
+                var height = this.height
+                delete img
 
-            // 开始读取图像数据
-            reader.readAsArrayBuffer(blob);
+                // 图像读取完成，执行回调     
+                var bufferReader = new FileReader();
+                bufferReader.onload = function(event) {           
+                    var buffer = new Buffer(bufferReader.result)
+                    sendFile(buffer, function(data) {
+                        if (data.code == 0 && data.image_url) {
+                            width = width / 2
+                            var html = '<img width=' + width + ' src="' + data.image_url + '">'
+                            document.execCommand('insertHTML', false, html)
+                        }
+                    })
+                }
+
+                // 读取图像数据
+                bufferReader.readAsArrayBuffer(blob);
+            };
+
+            var URLObj = window.URL || window.webkitURL;
+            img.src = URLObj.createObjectURL(blob);
         }
     })
 })
