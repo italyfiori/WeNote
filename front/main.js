d1286622b10aa2ed80b7509ce8de46e0746ff40d
yulu04 <yulu04@baidu.com>
2018-07-14 21:52:46 +0800

    update

--- front/main.js
+++ front/main.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
 // TODO: data路径可配置 done
 // TODO: range选择多个范围回退问题修复 done
 // TODO: 菜单栏优化: 增加删除提示、增加全部文件和回收站分类 done
-// TODO: 菜单栏优化: 增加静止拖拽节点和区域
+// TODO: 菜单栏优化: 增加静止拖拽节点和区域 done
+// TODO: 菜单栏图标优化: 
 // TODO: 增加快捷键(标题等)
 // TODO: 增加撤销功能
